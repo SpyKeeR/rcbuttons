b2bcb439bdd24d29483d78f6c6524ce2436bc700
SpyKeeR <203679352+SpyKeeR@users.noreply.github.com>
2025-10-30 14:26:31 +0100

    Mise à jour de la documentation du plugin RCButtons : ajout des informations de licence et des droits d'auteur dans plusieurs fichiers.

--- hook.php
+++ hook.php
@@ -1,6 +1,30 @@
 <?php
 /**
- * Hooks du plugin Remote Control Buttons
+ * -------------------------------------------------------------------------
+ * RCButtons plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of RCButtons.
+ *
+ * RCButtons is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RCButtons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RCButtons. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2025 by SpyKeeR.
+ * @license   GPLv3+ https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/SpyKeeR/rcbuttons
+ * -------------------------------------------------------------------------
  */
 
 /**
